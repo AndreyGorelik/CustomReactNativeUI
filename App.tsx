555d7728924fd8662cc9fa8b2c8fae73f8bcfbb7
Andrey Gorelik <bublic007@inbox.ru>
2023-06-05 23:16:08 +0300

    button, text, input text added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import TextInput from "./src/components/TextInput";
+import Text from "./src/components/Text";
+import Button from "./src/components/Button";
 export default function App() {
+  const [textInput, setTextInput] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={setTextInput}
+          value={textInput}
+          placeholder="Some placeholder"
+        />
+        <Text variant="h1">Header 1</Text>
+        <Text variant="h2">Header 2</Text>
+        <Text variant="h3">Header 3</Text>
+        <Text>standart text</Text>
+        <Button
+          title="standart width button"
+          onPress={() => console.log("hello")}
+        />
+        <Button
+          title="with width prop button"
+          width={"100%"}
+          onPress={() => console.log("hello")}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingVertical: 50,
+    paddingHorizontal: 15,
   },
 });
---
+++ src/components/Text.tsx
@@ -0,0 +1,47 @@
+import { Text as NativeText, StyleSheet } from "react-native";
+
+type TextVariant = "h1" | "h2" | "h3";
+
+interface CustomText {
+  children: string;
+  variant?: TextVariant;
+}
+
+function Text({ children, variant, ...rest }: CustomText) {
+    
+  const settings = {
+    fontSize: 15,
+    marginVertical: 5,
+  };
+
+  console.log(variant);
+
+  switch (variant) {
+    case "h1":
+      settings.fontSize = 40;
+      settings.marginVertical = 15;
+      break;
+    case "h2":
+      settings.fontSize = 30;
+      settings.marginVertical = 13;
+      break;
+    case "h3":
+      settings.fontSize = 25;
+      settings.marginVertical = 11;
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <NativeText style={[styles.text, settings]} {...rest}>
+      {children}
+    </NativeText>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {},
+});
+
+export default Text;
